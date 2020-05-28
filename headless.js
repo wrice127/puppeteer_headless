f7e3cacd8278949142d2d0599cea564824831771
Jae Hyuk Kwak <wrice127@gmail.com>
2020-05-28 09:35:32 -0700

    Initial check in

---
+++ headless.js
@@ -0,0 +1,44 @@
+const puppeteer = require( "puppeteer-core" );
+const net = require( "net" );
+const process = require( "process" );
+
+const port = process.argv[2] ?? 9222;
+
+const portInUse = function( port, callback ) {
+	var server = net.createServer( function( socket ) {
+		socket.write( "Echo server\r\n" );
+		socket.pipe( socket );
+	} );
+
+	server.listen( port, "127.0.0.1" );
+	server.on( "error", function ( e ) {
+		callback( true );
+	} );
+	server.on('listening', function (e) {
+		server.close();
+		callback(false);
+	} );
+};
+
+portInUse( port, function( inUse ) {
+	if ( inUse ) {
+		console.log( "Port(" + port + ") is already in use." );
+		process.exit( 1 );
+	} else {
+		puppeteer.launch( {
+			executablePath: "/usr/bin/chromium-browser",
+			args: [ "--remote-debugging-port=9222" ]
+		} ).then( browser => {
+			if ( browser )
+				console.log( "Browser started." );
+			else {
+				console.log( "Browser didn't start." );
+				process.exit( 1 );
+			}
+		} ).catch( () => {
+			console.log( "Browser didn't start." );
+			process.exit( 2 );
+		} );
+	}
+} );
+
